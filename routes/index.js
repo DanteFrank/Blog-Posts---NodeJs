9ec8bb08a61a1ecff7f650d3400fb9d5dd0779d6
DanteFrank <davidfrankoziwo@gmail.com>
2020-07-19 20:08:21 +0100

    Added Blog Model and error

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest} = require('../middleware/auth')
 
+const Blog = require('../models/Blog')
+
 
 //@decs Login Page
 //@route GET /
@@ -14,10 +16,20 @@ router.get('/', ensureGuest, (req, res) => {
 
 //@decs Dashboard Page
 //@route  /dashboaard
-router.get('/dashboard', ensureAuth, (req, res) => {
-    res.render('dashboard', {
-        name: req.user.firstName,
-    })
+router.get('/dashboard', ensureAuth, async (req, res) => {
+
+    try {
+        const blogs = await Blog.find( { user: req.user.id}).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            blogs
+        })
+    } catch (err) {
+        console.err(err)
+        res.render(error/500)
+    }
+
+   
 })
 
 
