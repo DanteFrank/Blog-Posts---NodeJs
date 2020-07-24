f048ed6bf82edb537e9ab3f7d2df4eb4b97ade99
DanteFrank <davidfrankoziwo@gmail.com>
2020-07-24 23:31:02 +0100

    App Complete

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
@@ -25,16 +26,32 @@ const app = express()
 app.use(express.urlencoded({ extended: false}))
 app.use(express.json())
 
+
+//Method- Override For Put Request
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
+
 //Morgan Login
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
 //Handlebar Helpers
-const { formatDate} = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon} = require('./helpers/hbs')
 
 //Handlesbars
-app.engine('.hbs', exphbs({ helpers: { formatDate,}, defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs({ 
+    helpers: { formatDate, stripTags, truncate, editIcon},
+     defaultLayout: 'main', 
+     extname: '.hbs'
+    }));
 app.set('view engine', '.hbs');
 
 
@@ -50,6 +67,13 @@ app.use(session({
 //Passport Middleware 
 app.use(passport.initialize())
 app.use(passport.session())
+
+//Set Global user var
+app.use((req, res, next) => {
+    res.locals.user = req.user || null
+    next()
+})
+
 
 
 //Static Folders
--- routes/blogs.js
+++ routes/blogs.js
@@ -24,6 +24,137 @@ router.post('/', ensureAuth, async (req, res) => {
     }
 })
 
+//@decs Show Public Blog Posts
+//@route GET /blogs
+router.get('/', ensureAuth, async (req, res) => {
+    try {
+        const blogs = await Blog.find({ status: 'public' })
+            .populate('user')
+            .sort({ createdAt: 'desc' })
+            .lean()
+        res.render('blogs/index', {
+            blogs,
+        })
+    } catch (err) {
+        console.err(err)
+        res.render('error/500')
+    }
+})
+
+
+//@decs Show Single Blog Page
+//@route GET /blogs/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id)
+            .populate('user')
+            .lean()
+        if(!blog) {
+            return res.render('error/404')
+        }
+
+        res.render('blogs/show', {
+            blog,
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/404')
+    }
+})
+
+
+
+//@decs Show Edit Page
+//@route GET /edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    try {
+        const blog = await Blog.findOne({
+            _id: req.params.id
+        }).lean()
+    
+        if (!blog) {
+            return res.render('error/404')
+        }
+    
+        if (blog.user != req.user.id) {
+            res.redirect('/blogs')
+        } else {
+            res.render('blogs/edit', {
+                blog,
+            })
+        }
+    } catch (error) {
+        console.error(err)
+        return res.render('error/500')
+    }
+   
+})
+
+
+//@decs Update Blog Post
+//@route PUT /blogs/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+    try {
+        const blog = await Blog.findById(req.params.id).lean()
+
+    if (!blog) {
+        return res.render('error/404')
+    }
+
+    if (blog.user != req.user.id) {
+        res.redirect('/blogs')
+    } else {
+        const blog = await Blog.findOneAndUpdate({ _id: req.params.id}, req.body, {
+            new: true,
+            runValidators: true,
+        })
+        res.redirect('/dashboard')
+    }
+    } catch (error) {
+        console.error(err)
+        return res.render('error/500')
+    }
+   
+
+})
+
+
+//@decs Delete Blog Post
+//@route DELETE /blogs/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        await Blog.remove({ _id: req.params.id})
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+    }
+})
+
+
+//@decs Show From User
+//@route GET /blogs/user/userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const blogs = await Blog.find({
+            user: req.params.userId,
+            status: 'public'
+        })
+        .populate('user')
+        .lean()
+
+        res.render('blogs/index', {
+            blogs,
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
+
+
+
+
 
 
 module.exports = router
